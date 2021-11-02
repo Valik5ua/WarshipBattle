c095b795747fc72a588049d7baa5f8f5f589cbe3
Daniel M <dashingdaniel05@gmail.com>
2021-11-01 21:41:08 -0400

    Animation finished

--- ClueField.h
+++ ClueField.h
@@ -7,4 +7,7 @@ public:
 	ClueField(const unsigned int StartX, const unsigned int StartY);
 	void Draw() override;
 	bool Click(POINT& coordinates) override { return false; }
+public:
+	float startX;
+	float startY;
 };
--- StatusField.h
+++ StatusField.h
@@ -4,7 +4,10 @@
 class StatusField : public Field
 {
 public:
-	StatusField(const unsigned int StartX, const unsigned int StartY) :Field(StartX, StartY) {}
+	StatusField(const unsigned int startX, const unsigned int startY) :startX(startX), startY(startY) {}
 	void Draw() override;
 	bool Click(POINT& coordinates) override { return false; }
+public:
+	float startX;
+	float startY;
 };
